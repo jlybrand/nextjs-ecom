6e749b48aca9230eba9c175601097085a09048af
jason <jasonlybrand@icloud.com>
2023-03-21 10:35:23 -0400

    added banner component and style

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import Header from "@/components/Header";
+import Banner from "@/components/Banner";
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-gray-100">
       <Head>
         <title>Next.js Store</title>
         <meta name="description" content="e-commerce store" />
@@ -11,6 +12,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-    </>
+
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner />
+      </main>
+    </div>
   );
 }
