787e7e7f35bb0de43e171cbc61ae738da7e9d30c
jason <jasonlybrand@icloud.com>
2023-03-21 23:55:09 -0400

    added hero icons and styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Image from "next/image";
-import { ShoppingCartIcon } from "@heroicons/react/20/solid";
+import {
+  ShoppingCartIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/20/solid";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useRouter } from "next/router";
 
@@ -29,20 +32,7 @@ function Header() {
             id=""
           />
           <div className="search-icon h-12 p-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-4 h-4"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-              />
-            </svg>
+            <MagnifyingGlassIcon className="w-4" />
           </div>
         </div>
 
@@ -63,27 +53,10 @@ function Header() {
             onClick={() => router.push("checkout")}
             className="relative link flex items-center"
           >
-            <span className="absolute right-0 top-0 md:right-5 h-4 w-4 bg-yellow-400 rounded-full font-bold text-black text-center">
+            <span className="absolute right-0 top-0 h-4 w-4 bg-yellow-400 rounded-full font-bold text-black text-center">
               3
             </span>
-            <ShoppingCartIcon className="w-8" />
-            {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="shopping-cart-icon w-8"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-              />
-            </svg> */}
-            <p className="hidden md:inline font-extrabold md:text-sm mt-2">
-              Cart
-            </p>
+            <ShoppingCartIcon className="w-10" />
           </div>
         </div>
       </div>
