94bba594370e41dd06e69044cd8dd891c93510b2
jason <jasonlybrand@icloud.com>
2023-03-27 15:26:51 -0400

    updated config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  mode: "jit",
-  purge: [
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: false,
   theme: {
     extend: {
       colors: {
