28293884507bfa4e8349399d39436a07292f8b16
jason <jasonlybrand@icloud.com>
2023-03-28 08:22:52 -0400

    added webhook for the checkout.session.completed event

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNghWA4OpgfUbuZ37eclsSkQjsSLBnLVw",
+  authDomain: "next-ecom-bad0b.firebaseapp.com",
+  projectId: "next-ecom-bad0b",
+  storageBucket: "next-ecom-bad0b.appspot.com",
+  messagingSenderId: "648410119584",
+  appId: "1:648410119584:web:877722270f9353493838c5",
+  measurementId: "G-8P2WZH78RK",
+};
