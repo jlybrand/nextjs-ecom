619eea3fb68e3fd4eef7cdd812281f934962fae1
jason <jasonlybrand@icloud.com>
2023-03-28 08:30:52 -0400

    added orders page

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,7 +1,34 @@
+import Header from "@/components/Header";
+import { CheckCircleIcon } from "@heroicons/react/20/solid";
+import { useRouter } from "next/router";
 import React from "react";
 
 function Success() {
-  return <div>Success</div>;
+  const router = useRouter();
+  return (
+    <div className="bg-blue-200 h-screen">
+      <Header />
+      <main className="max-w-screen-lg mx-auto">
+        <div className="flex flex-col p-10 bg-white">
+          <div className="flex items-center justify-center space-x-2 mb-5">
+            <CheckCircleIcon className="text-green-500 h-10" />
+            <h1 className="text-3xl">Order Placed</h1>
+          </div>
+          <p>
+            Thank you for shopping with us! You'll receive a confirmation email
+            once your order has shipped. To check the status of your order,
+            please click the link below.
+          </p>
+          <button
+            onClick={() => router.push("/orders")}
+            className="add-button mt-8"
+          >
+            Go to My Orders
+          </button>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Success;
